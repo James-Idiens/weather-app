fc1250501b163a61568b4e5bd8803d992c542f5e
James Idiens <jidiens@outlook.com>
2023-06-27 18:00:04 +1200

    error message for bad request

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -6,11 +6,19 @@ export default function App() {
   const [query, setQuery] = useState('')
   const [weather, setWeather] = useState<Current | null>(null)
   const [location, setLocation] = useState<Location | null>(null)
+  const [error, setError] = useState<string | null>(null)
 
   const handleSearch = async () => {
-    const weatherData = await getWeather(query)
-    setWeather(weatherData.current)
-    setLocation(weatherData.location)
+    try {
+      const weatherData = await getWeather(query)
+      setWeather(weatherData.current)
+      setLocation(weatherData.location)
+      setError(null) // Reset the error state if successful
+    } catch (error) {
+      setWeather(null)
+      setLocation(null)
+      setError('Whoops! Something went wrong 😬')
+    }
   }
 
   return (
@@ -22,12 +30,14 @@ export default function App() {
         placeholder="Enter a location"
       />
       <button onClick={handleSearch}>Search</button>
-      {weather && location && (
+      {error && <p>{error}</p>}
+      {weather && location && !error && (
         <>
           <h2>Current Weather</h2>
           <p>{location.name}</p>
           <p>{Math.round(weather.temp_c)}°C</p>
           <p>{weather.condition.text}</p>
+          <img src={weather.condition.icon} alt="Weather Icon" />
         </>
       )}
     </div>
