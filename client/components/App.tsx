b27f53136da1787025cb7406bf6b18a71627b2be
James Idiens <jidiens@outlook.com>
2023-07-04 11:04:50 +1200

    country now displayed

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -22,24 +22,39 @@ export default function App() {
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Enter a location"
-      />
-      <button onClick={handleSearch}>Search</button>
-      {error && <p>{error}</p>}
-      {weather && location && !error && (
-        <>
-          <h2>Current Weather</h2>
-          <p>{location.name}</p>
-          <p>{Math.round(weather.temp_c)}°C</p>
-          <p>{weather.condition.text}</p>
-          <img src={weather.condition.icon} alt="Weather Icon" />
-        </>
-      )}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
+        <div className="flex flex-col items-center">
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter a location"
+            aria-label="weather-search"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-full"
+          />
+          <button
+            onClick={handleSearch}
+            className="mt-4 ml-2 px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            Search
+          </button>
+        </div>
+        {error && <p>{error}</p>}
+        {weather && location && !error && (
+          <div className="mt-4 p-4 bg-gray-200 rounded">
+            <div className="flex flex-col items-center">
+              <h2 className="text-lg font-semibold">Current Weather</h2>
+              <p>
+                {location.name}, {location.country}
+              </p>
+              <p>{Math.round(weather.temp_c)}°C</p>
+              <p>{weather.condition.text}</p>
+              <img src={weather.condition.icon} alt="Weather Icon" />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
