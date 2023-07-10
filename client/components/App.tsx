31f0fbe8afe97b7da0c60c78fb117cc2616fc347
James Idiens <jidiens@outlook.com>
2023-07-10 16:20:10 +1200

    app changes colour with conditions

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -22,11 +22,35 @@ export default function App() {
     }
   }
 
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch()
+    }
+  }
+
+  const getWeatherDivClass = () => {
+    if (weather && weather.condition.text) {
+      const condition = weather.condition.text.toLowerCase()
+
+      if (condition.includes('sunny')) {
+        return 'bg-gradient-to-r from-blue-500 to-blue-300'
+      } else if (condition.includes('cloud')) {
+        return 'bg-gradient-to-r from-gray-500 to-gray-300'
+      } else if (condition.includes('rain')) {
+        return 'bg-gradient-to-b from-violet-800 to-violet-600'
+      }
+    }
+
+    return 'bg-gradient-to-r from-blue-500 to-blue-300'
+  }
+
   return (
     <div className="relative min-h-screen bg-gray-100">
       <Wallpaper />
       <div className="absolute inset-0 flex flex-col items-center justify-center pt-16">
-        <div className="max-w-md w-full bg-gradient-to-r from-blue-500 to-blue-300 rounded-xl shadow-lg p-8">
+        <div
+          className={`max-w-md w-full ${getWeatherDivClass()} rounded-xl shadow-lg p-8`}
+        >
           <div className="flex flex-col items-center">
             <input
               type="text"
@@ -35,6 +59,7 @@ export default function App() {
               placeholder="Enter a location"
               aria-label="weather-search"
               className="mt-4 px-4 py-2 bg-white text-black rounded-full"
+              onKeyDown={handleKeyPress}
             />
             <button
               onClick={handleSearch}
